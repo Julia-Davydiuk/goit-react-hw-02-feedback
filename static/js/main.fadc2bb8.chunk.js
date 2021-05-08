480bf516adea72795d7dac8247ab18db882101d6
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2021-05-08 18:22:16 +0200

    Updates

---
+++ static/js/main.fadc2bb8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],[function(e,c){}],[[0,1]]]);
+//# sourceMappingURL=main.fadc2bb8.chunk.js.map
